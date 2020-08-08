fa04bd5d081ebde8ad918b99681c75139df053f2
PC259 <ckr344.225@gmail.com>
2020-08-08 12:40:58 +0900

    redux-sample app create

--- redux-sample/src/components/Counter/container.jsx
+++ redux-sample/src/components/Counter/container.jsx
@@ -1,3 +1,28 @@
+import { connect } from 'react-redux';
+import { increment, decrement } from '../../redux/count/actions';
 import Counter from "./presentation";
 
-export default Counter;
+// storeからどのstateを引っ張ってくるかを定義する
+// "State"を"ToProps" >> State情報をPropsとして扱うことができる
+const mapStateProps = ({ count }) => ({ count });
+
+// ^下記のようにも書ける
+// const mapStateProps = state => {
+//   return { count: state.count };
+// };
+
+// どんなdispatcherをpropsに渡すかを定義する
+// "Dispatch(Action関数)"を"ToProps" >> Action関数をPropsとして扱うことができる
+const mapDispatchProps = dispatch => ({
+  increment: count => {
+    dispatch(increment(count));
+  },
+  decrement: count => {
+    dispatch(decrement(count));
+  }
+});
+
+export default connect(
+  mapStateProps,
+  mapDispatchProps
+)(Counter);
