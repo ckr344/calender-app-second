e39028a0fed705643ef05c28762d73580639e43b
PC259 <ckr344.225@gmail.com>
2020-08-10 13:47:29 +0900

    カレンダーのUIを実装

---
+++ front/src/components/Navigation/container.jsx
@@ -0,0 +1,12 @@
+import Navigation from './presentation';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({});
+const margeProps = (stateProps, dispatchProps) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  margeProps
+)(Navigation);
